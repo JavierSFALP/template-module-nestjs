405707f265e1eac1532054af12571ffbce351e32
unknown <javier.salamanca@falp.org>
2025-02-01 21:15:22 -0300

    test: test package installed

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LoggerModule } from './example-module-lib';
 import { ExamplelibModule } from 'example-lib-1'
+import { ExamplelibModule as npmModule} from '@javiersfalp/template-module-nodejs'
 
 export const loggerModule = LoggerModule.register({ logLevel: 'info' });
 
 @Module({
   imports: [
     loggerModule,
-    ExamplelibModule
+    ExamplelibModule,
+    npmModule
   ],
   controllers: [AppController],
   providers: [AppService],
