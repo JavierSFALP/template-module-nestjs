9f015795b3c26f2d91ed0d438387062e8459e0e1
unknown <javier.salamanca@falp.org>
2025-02-03 00:02:48 -0300

    fix(release version): new version

--- libs/example-lib/src/example-lib.service.ts
+++ libs/example-lib/src/example-lib.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class ExamplelibService {
   greet(): string {
-    return 'This is scoped example message'; 
+    return 'This is unscoped example message'; 
   }
 }
